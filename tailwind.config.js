faf84b1a23abf642fb22a598d75d8cdff3817fa9
Christopher Ray <cr525@gmail.com>
2025-05-05 21:11:59 -0400

    chore: add rounded-xl to Tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      borderRadius: {
+        'xl': '1rem',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
