7fad75cb573984a621459753b0e4632fd31779d6
Christopher Ray <cr525@gmail.com>
2025-05-06 13:09:43 -0400

    fix: install @tailwindcss/postcss, update PostCSS config & remove appDir flag

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enable the new App Router in Next.js 15+
-  appDir: true,
-
+module.exports = {
   typescript: {
     ignoreBuildErrors: true, // allow production build despite type errors
   },
@@ -17,6 +14,4 @@ const nextConfig = {
       },
     ],
   },
-};
-
-module.exports = nextConfig; 
+}; 
