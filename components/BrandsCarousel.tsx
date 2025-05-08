d446cf295854c98d5ea2fdd7e6d9f150995b08d1
Christopher Ray <cr525@gmail.com>
2025-05-07 21:39:50 -0400

    feat: update brand carousel to use static logo array from Supabase storage

--- components/BrandsCarousel.tsx
+++ components/BrandsCarousel.tsx
@@ -1,11 +1,5 @@
-import fs from "fs";
-import path from "path";
 import BrandsCarouselClient from "./BrandsCarouselClient";
 
 export default function BrandsCarousel() {
-  const files = fs
-    .readdirSync(path.join(process.cwd(), "public", "brands"))
-    .filter((f) => /\.(png|webp)$/i.test(f));
-
-  return <BrandsCarouselClient files={files} />;
+  return <BrandsCarouselClient files={[]} />;
 } 
